b224e91c61cb65c1e6a8f19551e95ebbb033bbac
Nicolas Amaya <nicolascomp@gmail.com>
2024-02-18 03:57:03 -0700

    Added all objects required for map creation and implemented terrain nature change over time

--- Scenes/Scripts/Lil_Rock_Type_1.gd
+++ Scenes/Scripts/Lil_Rock_Type_1.gd
@@ -1,24 +1,23 @@
 extends StaticBody2D
 
-@onready var animation = $Big_Rock_Type_1
+@onready var animation = $Lil_Rock_Type_1
 var health 
 
 func _ready():
-	animation = $Big_Rock_Type_1
+	animation = $Lil_Rock_Type_1
 	pass
 
 
 func _process(delta):
 	health = get_parent().get_parent().mainHealth
 	
-	#MAIN TREE rock
+	#MAIN ROCK 2
 	
-	if (health > 90):
+	if (health > 0 && health < 30):
 		animation.play("Stage_1")
-	elif (health > 76):
+	elif (health > 35 && health < 75):
 		animation.play("Stage_2")
-	elif(health > 51):
+	elif(health > 75):
 		animation.play("Stage_3")
-	elif(health > 0): 
-		animation.play("Stage_3")
+
 		
